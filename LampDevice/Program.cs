5bd2ff522426406f13373044a9154c563dfb802c
Frederick <frederick.wennborg@gmail.com>
2023-09-06 15:49:18 +0200

    Added Device services and NetworkManager

--- LampDevice/Program.cs
+++ LampDevice/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SharedLibrary.Handlers.Services;
+
+var device = new DeviceManager("HostName=fw-kyh-iothb.azure-devices.net;DeviceId=advanced_device;SharedAccessKey=WB3KAmx01yMMOJEJ5iXJs0S2gLlcI5+Az6qfar8FMzA=");
+
+device.Start();
