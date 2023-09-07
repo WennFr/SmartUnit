77e564b6c572c5ee833f935964449aeaadb6c207
Frederick <frederick.wennborg@gmail.com>
2023-09-07 11:19:08 +0200

    added send telemetryData from Device to SharedLibrary

--- LampDevice/Program.cs
+++ LampDevice/Program.cs
@@ -12,3 +12,6 @@ var telemetryData = new TelemetryDataModel()
 
 var device = new DeviceManager<TelemetryDataModel>("HostName=fw-kyh-iothb.azure-devices.net;DeviceId=lamp_device;SharedAccessKey=V+CuvgjqsYVonjXAPpLBZE8GsqIQVo5RMzHU97Cw3tM=", telemetryData);
 device.Start();
+
+Console.WriteLine("Press [Enter] to close application");
+Console.ReadKey();
