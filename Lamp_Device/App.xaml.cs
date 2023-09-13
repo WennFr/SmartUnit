b2f5d57b4269015f0586e50c864fd85a4efe01b7
Frederick <frederick.wennborg@gmail.com>
2023-09-13 16:09:29 +0200

    installed necessary nigets created App connections

---
+++ Lamp_Device/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lamp_Device
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public IHost? AppHost { get; set; }
+
+
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((config, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                    services.AddSingleton(new DeviceManager("HostName=fw-kyh-iothb.azure-devices.net;DeviceId=wpf_device;SharedAccessKey=jrQL6oH1AXscyYKAtTx7MEXgpwpDxUKeWX2UBIiu22Y="));
+                })
+                .Build();
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();   // med DI
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+    }
+}
