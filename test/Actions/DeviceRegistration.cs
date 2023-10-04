090e8b493c563df08835e0e1e8d64e5184db7cc5
Frederick <frederick.wennborg@gmail.com>
2023-10-04 10:26:25 +0200

    changes

---
+++ test/Actions/DeviceRegistration.cs
@@ -0,0 +1,64 @@
+using System.Net;
+using AzureFunctions.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctions.Actions
+{
+    public class DeviceRegistration
+    {
+        private readonly IotHubManager _iotHubManager;
+        public DeviceRegistration(IotHubManager iotHubManager)
+        {
+            _iotHubManager = iotHubManager;
+        }
+
+        [Function("DeviceRegistration")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function,  "post")] HttpRequestData req)
+        {
+            var deviceId = req.Query["deviceId"];
+
+            // gör sdå att deviceid alltid slutar på _device
+
+
+            if (!string.IsNullOrEmpty(deviceId))
+            {
+                var device = await _iotHubManager.GetDeviceAsync(deviceId);
+                device ??= await _iotHubManager.RegisterDeviceAsync(deviceId);
+
+                if (device != null)
+                {
+                    var connectionString = _iotHubManager.GenerateConnectionString(device);
+                    if (!string.IsNullOrEmpty(connectionString))
+                    {
+                        return GenerateHttpResponse(req, HttpStatusCode.OK, connectionString);
+                    }
+                    else
+                    {
+                        return GenerateHttpResponse(req, HttpStatusCode.BadRequest, "An error occured! Connectionstring was not created.");
+                    }
+                }
+
+                return GenerateHttpResponse(req, HttpStatusCode.BadRequest, "An error occured! Parameter deviceId is required.");
+            }
+
+            return null!;
+        }
+
+
+
+        private HttpResponseData GenerateHttpResponse(HttpRequestData req, HttpStatusCode statusCode, string content)
+        {
+
+            var response = req.CreateResponse(statusCode);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString(content);
+
+            return response;
+        }
+    }
+
+
+}
