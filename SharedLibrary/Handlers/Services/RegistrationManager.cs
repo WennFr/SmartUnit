c062387d276e1332624162852bc5732f52124399
Frederick <frederick.wennborg@gmail.com>
2023-10-12 10:40:28 +0200

    published to azure

--- SharedLibrary/Handlers/Services/RegistrationManager.cs
+++ SharedLibrary/Handlers/Services/RegistrationManager.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Devices.Client;
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.Shared;
 using System;
 using System.Collections.Generic;
@@ -13,12 +14,12 @@ namespace SharedLibrary.Handlers.Services
         private string _connectionString = string.Empty;
         private DeviceClient deviceClient = null!;
 
-        public async Task<string> RegisterDevice(string deviceName, string deviceType)
+        public async Task<string> RegisterDevice(string deviceId, string deviceType)
         {
             try
             {
                 using var httpClient = new HttpClient();
-                var result = await httpClient.PostAsync($"http://localhost:7193/api/DeviceRegistration?deviceId={deviceName}", null!);
+                var result = await httpClient.PostAsync($"https://fw-smart-af.azurewebsites.net/api/DeviceRegistration?deviceId={deviceId}&code=pfwA54Y0hVDgQ_XJpOJLsiGDOnKykwXwEYg4SyZk1aUbAzFuyXhLGw==", null!);
                 _connectionString = await result.Content.ReadAsStringAsync();
 
                 deviceClient = DeviceClient.CreateFromConnectionString(_connectionString, TransportType.Mqtt);
